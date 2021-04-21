04d1a20c31e6c8babcb773667aa3717587596ea8
Koharu gotou <smspring0426@gmail.com>
2021-04-21 12:14:08 +0900

    test: Update commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,20 @@
-module.exports = {extends: ['@commitlint/config-conventional']};
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        ':aaa',
+        ':bug:',
+        ':art:',
+        ':tada:',
+        ':bento:',
+        'test'
+      ]
+    ],
+    'type-case': [0, 'never', 'lower-case'],
+    'subject-case': [2, 'always', 'sentence-case'],
+    'subject-empty': [0, 'never']
+  }
+};
